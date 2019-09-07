a35635590cdaf4da0aaf5da3e71afb164d155853
Daniela P. Riesgo <daniela@evolve.xyz>
2019-09-07 14:28:26 -0300

    Add validation

--- app/src/main/java/com/example/matematicamente/login/LoginActivity.java
+++ app/src/main/java/com/example/matematicamente/login/LoginActivity.java
@@ -6,6 +6,8 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 
 import com.example.matematicamente.R;
@@ -26,16 +28,45 @@ public class LoginActivity extends AppCompatActivity {
         ageInput = findViewById(R.id.age_input);
         button = findViewById(R.id.continue_button);
 
+        button.setEnabled(false);
+        nameInput.addTextChangedListener(createInputValidator());
+        ageInput.addTextChangedListener(createInputValidator());
+
         button.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("ApplySharedPref")
             @Override
             public void onClick(View view) {
                 SharedPreferences pref = getSharedPreferences(getString(R.string.app_name), Context.MODE_PRIVATE);
                 SharedPreferences.Editor editor = pref.edit();
-                editor.putString(getString(R.string.name_input_text), nameInput.getText().toString());
-                editor.putString(getString(R.string.age_input_text), ageInput.getText().toString());
+                editor.putString(getString(R.string.name_input_text), getText(nameInput));
+                Editable age = ageInput.getText();
+                editor.putString(getString(R.string.age_input_text), getText(ageInput));
                 editor.commit();
             }
         });
+    }
+
+    private String getText(TextInputEditText input) {
+        Editable textEdit = input.getText();
+        return textEdit == null ? "" : textEdit.toString();
+    }
+
+    private TextWatcher createInputValidator() {
+        return new TextWatcher() {
+
+            public void afterTextChanged(Editable s) {
+            }
+
+            public void beforeTextChanged(CharSequence s, int start,
+                                          int count, int after) {
+            }
+
+            public void onTextChanged(CharSequence s, int start,
+                                      int before, int count) {
+                boolean isNameEmpty = getText(nameInput).equals("");
+                boolean isAgeEmpty = getText(ageInput).equals("");
+                button.setEnabled(!isNameEmpty && !isAgeEmpty);
+            }
+        };
     }
 }

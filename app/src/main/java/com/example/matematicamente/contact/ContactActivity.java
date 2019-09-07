5a7e3f1dcfe7f9b4e53376ea246b02c502e10c32
gfusca <gfuscax@gmail.com>
2019-09-07 15:18:44 -0300

    Changes:   - create contact form   - use mailto to send email with contact form message

--- app/src/main/java/com/example/matematicamente/contact/ContactActivity.java
+++ app/src/main/java/com/example/matematicamente/contact/ContactActivity.java
@@ -1,16 +1,33 @@
 package com.example.matematicamente.contact;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import com.example.matematicamente.R;
+import com.example.matematicamente.config.Configuration;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ContactActivity extends AppCompatActivity {
 
+    EditText emailBodyText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
+        emailBodyText = findViewById(R.id.contactText);
+    }
+
+    public void onSendClick(View view) {
+        // enviar email utilizando mailto
+        Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                "mailto", Configuration.EMAIL_TO, null));
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, Configuration.EMAIL_SUBJECT);
+        emailIntent.putExtra(Intent.EXTRA_TEXT, emailBodyText.getText());
+        startActivity(Intent.createChooser(emailIntent, "Send email..."));
     }
 }

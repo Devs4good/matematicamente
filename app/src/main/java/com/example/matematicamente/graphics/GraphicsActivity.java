9b4b8120f56745e6db42cb9e447ebf609ec32b98
Daniela P. Riesgo <daniela@evolve.xyz>
2019-09-07 18:45:43 -0300

    Add Game

--- app/src/main/java/com/example/matematicamente/graphics/GraphicsActivity.java
+++ app/src/main/java/com/example/matematicamente/graphics/GraphicsActivity.java
@@ -4,35 +4,146 @@ import android.animation.ValueAnimator;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.graphics.Color;
+import android.util.Log;
+import android.util.Pair;
+import android.widget.Toast;
 
 import com.example.matematicamente.R;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+import com.jjoe64.graphview.series.PointsGraphSeries;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class GraphicsActivity extends AppCompatActivity implements View.OnTouchListener {
+public class GraphicsActivity extends AppCompatActivity {
 
     private View map;
     private View cursorView;
+
+    GraphView graph;
+    MyGraphSeries<DataPoint> invisibleSeries;
+    MyGraphSeries<DataPoint> originSeries;
+    LineGraphSeries<DataPoint> lineSeries;
+    PointsGraphSeries<DataPoint> pointSeries;
+    int lastCorrectMove = -1;
+    DataPoint[] points;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graphics);
-        map = findViewById(R.id.map);
-        map.setOnTouchListener(this);
         cursorView = findViewById(R.id.cursor);
+        graph = findViewById(R.id.graph);
+
+        setUpPoints();
+        setUpGraph();
+        setUpSeries();
+//        cursorView.setVisibility(View.INVISIBLE);
     }
 
-
     @Override
-    public boolean onTouch(View view, MotionEvent motionEvent) {
-        float dx = motionEvent.getX();
-        float dy = motionEvent.getY();
-        moveCursorCoordinate(dx, dy);
+    protected void onResume() {
+        super.onResume();
+//        setUpCursor();
+    }
+
+    private void setUpPoints() {
+        points = new DataPoint[]{
+                new DataPoint(-4, 2),
+                new DataPoint(-3, 4),
+                new DataPoint(-1, 0),
+                new DataPoint(1, -1),
+                new DataPoint(2, 3)
+        };
+    }
+
+    private boolean hasFinished() {
+        return lastCorrectMove == points.length - 1;
+    }
+
+    private void setUpGraph() {
+        graph.setTitle(getString(R.string.graphics_option_title));
+        graph.setTitleTextSize(32);
+        graph.getViewport().setXAxisBoundsManual(true);
+        graph.getViewport().setYAxisBoundsManual(true);
+        graph.getViewport().setMinX(-5);
+        graph.getViewport().setMaxX(5);
+        graph.getViewport().setMinY(-5);
+        graph.getViewport().setMaxY(5);
+    }
+
+    private void setUpSeries() {
+        invisibleSeries = new MyGraphSeries<>(points);
+        invisibleSeries.setColor(Color.TRANSPARENT);
+        graph.addSeries(invisibleSeries);
+        originSeries = new MyGraphSeries<>(new DataPoint[]{new DataPoint(0, 0)});
+        originSeries.setColor(Color.TRANSPARENT);
+        graph.addSeries(originSeries);
+        pointSeries = new PointsGraphSeries<>();
+        graph.addSeries(pointSeries);
+        lineSeries = new LineGraphSeries<>();
+        graph.addSeries(lineSeries);
+    }
+
+//    private void setUpCursor() {
+//        cursorView.setVisibility(View.VISIBLE);
+//        Pair<Float, Float> origin = originSeries.getLocationForPoint(new DataPoint(0, 0));
+//        cursorView.setX(origin.first);
+//        cursorView.setY(origin.second);
+//    }
+
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        int index = event.getActionIndex();
+        int action = event.getActionMasked();
+        int[] contentLocation = new int[2];
+        graph.getLocationOnScreen(contentLocation);
+        float originalXPosition = event.getX();
+        float originalYPosition = event.getY();
+        Log.d("GRAPH", String.format("Touch will be dispatched in (%f;%f)", originalXPosition, originalYPosition));
+        float xPosition = event.getX() - contentLocation[0];
+        float yPosition = event.getY() - contentLocation[1];
+        Log.d("GRAPH", String.format("Coordinates after transformed (%f;%f)", xPosition, yPosition));
+
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                break;
+            case MotionEvent.ACTION_MOVE:
+                break;
+            case MotionEvent.ACTION_UP:
+                Pair<Double, Double> touchCoordinates = invisibleSeries.getPointForLocation(xPosition, yPosition);
+                if (touchCoordinates == null) {
+                    Toast.makeText(this, "No point, try again.", Toast.LENGTH_SHORT).show();
+                    break;
+                }
+                DataPoint dataPoint = new DataPoint(touchCoordinates.first, touchCoordinates.second);
+                for (int i = 0; i < points.length; i++) {
+                    DataPoint currentPoint = points[i];
+                    if (currentPoint.getX() == dataPoint.getX() && currentPoint.getY() == dataPoint.getY()) {
+                        // currentPoint is the Point tapped on
+                        if (i == lastCorrectMove + 1) {
+                            // currentPoint is the correct point to tap
+                            Toast.makeText(this, "Correct!!", Toast.LENGTH_LONG).show();
+                            lastCorrectMove = i;
+                            showCorrectLineTo(dataPoint);
+                            moveCursorCoordinate(xPosition, yPosition);
+                            if (hasFinished()) {
+                                succeededGame();
+                            }
+                        } else {
+                            Toast.makeText(this, "Wrong point, try again.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                break;
+        }
         return true;
     }
 
-    protected void moveCursorCoordinate(float X, float Y) {
+    private void moveCursorCoordinate(float X, float Y) {
         int mDuration = 1000;
         float previousX = cursorView.getX();
         float previousY = cursorView.getY();
@@ -40,7 +151,7 @@ public class GraphicsActivity extends AppCompatActivity implements View.OnTouchL
         vaX.setDuration(mDuration);
         vaX.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             public void onAnimationUpdate(ValueAnimator animation) {
-                cursorView.setX((float) animation.getAnimatedValue());
+                setCursorX((float) animation.getAnimatedValue());
             }
         });
         vaX.start();
@@ -48,9 +159,27 @@ public class GraphicsActivity extends AppCompatActivity implements View.OnTouchL
         vaY.setDuration(mDuration);
         vaY.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             public void onAnimationUpdate(ValueAnimator animation) {
-                cursorView.setY((float) animation.getAnimatedValue());
+                setCursorY((float) animation.getAnimatedValue());
             }
         });
         vaY.start();
     }
+
+    private void setCursorX(float x) {
+        cursorView.setX(x - cursorView.getWidth() / 2f);
+    }
+
+    private void setCursorY(float y) {
+        cursorView.setX(y - cursorView.getHeight() / 2f);
+    }
+
+    private void showCorrectLineTo(DataPoint point) {
+        lineSeries.appendData(point, false, points.length);
+        pointSeries.appendData(point, false, points.length);
+    }
+
+    private void succeededGame() {
+        Toast.makeText(this, "YOU WON!!!!", Toast.LENGTH_SHORT).show();
+    }
+
 }
---
+++ app/src/main/java/com/example/matematicamente/graphics/MyGraphSeries.java
@@ -0,0 +1,54 @@
+package com.example.matematicamente.graphics;
+
+import android.util.Log;
+import android.util.Pair;
+
+import com.jjoe64.graphview.series.DataPointInterface;
+import com.jjoe64.graphview.series.PointsGraphSeries;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MyGraphSeries<E extends DataPointInterface> extends PointsGraphSeries {
+
+    final float TOLERANCE = 20f;
+    private Map<Pair<Double, Double>, Pair<Float, Float>> logicPointsToRealPoints = new HashMap<>();
+    private Map<Pair<Float, Float>, Pair<Double, Double>> realPointsToLogicPoints = new HashMap<>();
+
+    public MyGraphSeries(E[] data) {
+        super(data);
+        init();
+    }
+
+    @Override
+    protected void registerDataPoint(float x, float y, DataPointInterface dp) {
+        super.registerDataPoint(x, y, dp);
+        Pair<Double, Double> logicPoint = new Pair<>(dp.getX(), dp.getY());
+        Pair<Float, Float> realPoint = new Pair<>(x, y);
+        logicPointsToRealPoints.put(logicPoint, realPoint);
+        realPointsToLogicPoints.put(realPoint, logicPoint);
+
+    }
+
+    public Pair<Float, Float> getLocationForPoint(E point) {
+        return logicPointsToRealPoints.get(new Pair<>(point.getX(), point.getY()));
+    }
+
+    public Pair<Double, Double> getPointForLocation(float x, float y) {
+        Log.d("GRAPH", String.format("Looking for (%f;%f)", x, y));
+        for (Map.Entry<Pair<Float, Float>, Pair<Double, Double>> entry : realPointsToLogicPoints.entrySet()) {
+            Pair<Float, Float> key = entry.getKey();
+            Pair<Double, Double> value = entry.getValue();
+            float realX = key.first;
+            float realY = key.second;
+            Log.d("GRAPH", String.format("Having (%f;%f)", realX, realY));
+            if (x < realX + TOLERANCE && x > realX - TOLERANCE
+                    && y < realY + TOLERANCE && y > realY - TOLERANCE) {
+                Log.d("GRAPH", String.format("Coincided !"));
+                return value;
+            }
+        }
+        return null;
+    }
+
+}

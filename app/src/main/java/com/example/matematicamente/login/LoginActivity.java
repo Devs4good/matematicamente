62a014e6318f66f7b44e53a4da8676a637f34514
Daniela P. Riesgo <daniela@evolve.xyz>
2019-09-07 11:58:04 -0300

    Initial commit

---
+++ app/src/main/java/com/example/matematicamente/login/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.matematicamente.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.matematicamente.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
